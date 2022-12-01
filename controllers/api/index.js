c8dc2d7d90a16e355b68fdb528801b4ecdee6b05
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-01 00:14:25 -0600

    i'm commenting things bit by bit to see if i can find what the error is

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const userRoutes = require('./user-routes');
 // const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+// router.use('/post', postRoutes);
+// router.use('/comment', commentRoutes);
 
 module.exports = router;
