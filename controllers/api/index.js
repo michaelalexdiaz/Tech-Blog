347a46d6a65a1e25f510180465a999a5223d56e4
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-01 00:19:29 -0600

    potential solution, changing file directories

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-// const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
+const commentRoutes = require('./api/comment-routes');
 
 router.use('/user', userRoutes);
-// router.use('/post', postRoutes);
-// router.use('/comment', commentRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
