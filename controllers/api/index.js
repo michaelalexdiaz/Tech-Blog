72dad3e10678012f48e54d192f6c715a48d51be8
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-01 00:29:16 -0600

    still trying

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./api/post-routes');
-const commentRoutes = require('./api/comment-routes');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./controllers/home-routes.js');
-const dashboardRoutes = require('./controllers/dashboard-routes.js');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
