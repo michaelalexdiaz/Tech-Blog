bbc2a88cc4bb5da94d79f8860e3b8783245657fd
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-01 00:12:33 -0600

    still testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 // const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+// const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
